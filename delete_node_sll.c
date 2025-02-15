144b4782b90aa3b7297586c01aee33f0951b7163
Anzil Hussain <143596975+anzilcode@users.noreply.github.com>
2025-02-15 16:58:32 +0530

    Update delete_node_sll.c

--- delete_node_sll.c
+++ delete_node_sll.c
@@ -3,36 +3,37 @@
 ### 1. Start
 
 ### 2. Define a structure `Node`
-   - Contains `data`
+   - Contains an integer `data`
    - Pointer `next` to the next node
 
 ### 3. Define `main()`
    - Initialize `head` as `NULL`
-   - Use `insertNode` to add nodes
+   - Use `insertNode` to add nodes with values `{10, 20, 30, 50, 40}`
+   - Print the linked list
    - Display a menu with options:
      1. Delete a node
      2. Print the list
      3. Exit
    - Perform operations based on user input
 
-### 4. Define `insertNode(head, data)`
-   - Create a new node
-   - Assign `data` to the new node
-   - If the list is empty, set `head` to the new node
+### 4. Define `insertNode(head, value)`
+   - Create a new node and assign `value` to it
+   - If the list is empty (`head == NULL`), set `head` to the new node
    - Otherwise, traverse to the last node and link the new node
 
-### 5. Define `deleteNode(head, key)`
-   - If `head` contains `key`, update `head` to `head->next` and free memory
-   - Otherwise, traverse the list to find `key`
-   - If found, update `prev->next` and free memory
+### 5. Define `deleteNode(head, value)`
+   - If `head` contains `value`, update `head` to `head->next` and free memory
+   - Otherwise, traverse the list to find `value`
+   - If found, update `prev->next` to `temp->next` and free `temp`
+   - If the element is **not found**, print a message
 
 ### 6. Define `printList(head)`
    - Traverse and print each node’s `data`
-   - Print `NULL` at the end
+   - Print `"NULL"` at the end
 
 ### 7. End
 
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
 
@@ -41,9 +42,9 @@ struct Node {
     struct Node* next;
 };
 
-void insertNode(struct Node** head, int data) {
+void insertNode(struct Node** head, int value) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = data;
+    newNode->data = value;
     newNode->next = NULL;
 
     if (*head == NULL) {
@@ -58,66 +59,65 @@ void insertNode(struct Node** head, int data) {
     temp->next = newNode;
 }
 
-void deleteNode(struct Node** head, int key) {
-    struct Node *temp = *head, *prev = NULL;
+void deleteNode(struct Node** head, int value) {
+    struct Node* temp = *head, *prev = NULL;
 
-    if (temp && temp->data == key) {
+    if (temp && temp->data == value) {
         *head = temp->next;
         free(temp);
-        printf("\n%d deleted successfully.\n", key);
+        printf("%d deleted successfully from the list\n", value);
         return;
     }
 
-    while (temp && temp->data != key) {
+    while (temp && temp->data != value) {
         prev = temp;
         temp = temp->next;
     }
 
-    if (!temp) {
-        printf("\nElement %d not found.\n", key);
+    if (temp == NULL) {
+        printf("%d not found in the list\n", value);
         return;
     }
 
     prev->next = temp->next;
     free(temp);
-    printf("\n%d deleted successfully.\n", key);
+    printf("Deleted successfully\n");
 }
 
 void printList(struct Node* head) {
-    printf("\nCurrent List: ");
-    while (head) {
-        printf("%d -> ", head->data);
-        head = head->next;
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
     }
     printf("NULL\n");
 }
 
 int main() {
     struct Node* head = NULL;
-    int choice, deleteValue;
-
+    int choice, value;
     clrscr();
 
     insertNode(&head, 10);
     insertNode(&head, 20);
     insertNode(&head, 30);
-    insertNode(&head, 40);
     insertNode(&head, 50);
+    insertNode(&head, 40);
+
+    printList(head);
 
     while (1) {
-        printf("\nMenu:");
-        printf("\n1. Delete a node");
-        printf("\n2. Print list");
-        printf("\n3. Exit");
-        printf("\nEnter your choice: ");
+        printf("\n1. Delete\n");
+        printf("2. Display\n");
+        printf("3. Exit\n");
+        printf("Enter the choice: ");
         scanf("%d", &choice);
 
         switch (choice) {
             case 1:
-                printf("\nEnter value to delete: ");
-                fflush(stdin);
-                scanf("%d", &deleteValue);
-                deleteNode(&head, deleteValue);
+                printf("Enter the value to delete: ");
+                scanf("%d", &value);
+                deleteNode(&head, value);
                 break;
 
             case 2:
@@ -125,14 +125,15 @@ int main() {
                 break;
 
             case 3:
-                printf("\nExiting program...\n");
                 return 0;
 
             default:
-                printf("\nInvalid choice! Try again.\n");
+                printf("Enter valid input...!\n");
         }
     }
 
     getch();
     return 0;
 }
+
+
