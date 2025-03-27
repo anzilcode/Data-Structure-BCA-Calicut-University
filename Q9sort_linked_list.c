927e1373b7bbe467f2f2463abe72cb815e80ff05
Anzil Hussain <143596975+anzilcode@users.noreply.github.com>
2025-03-27 18:18:05 +0530

    Q9sort_linked_list.c

--- Q9sort_linked_list.c
+++ Q9sort_linked_list.c
@@ -1,134 +1,118 @@
-## Sorting a Singly Linked List
+# Sorting a Singly Linked List
 
-### Algorithm
+## Algorithm
 
-### 1. Start
+### 1. Start  
 
-### 2. Define a structure `Node`
-   - Contains an integer `data`
-   - Pointer `next` to the next node
+### 2. Define a structure `Node`  
+   - Contains an integer `data`  
+   - Pointer `next` to the next node  
 
-### 3. Define `main()`
-   - Initialize `head` as `NULL`
-   - Insert elements into the linked list using `insertNode()`
-   - Print the linked list before sorting
-   - Call `bubbleSort(head)`
-   - Print the sorted linked list
+### 3. Define `main()`  
+   - Initialize `head` as `NULL`  
+   - Insert elements into the linked list using `insertNode()`  
+   - Print the linked list before sorting  
+   - Call `bubbleSort(head)`  
+   - Print the sorted linked list  
 
-### 4. Define `insertNode(head_ref, data)`
-   - Create a new node
-   - Assign `data` to the new node
-   - If the linked list is empty, set `head_ref` to the new node
-   - Otherwise, traverse to the end and insert the new node
+### 4. Define `insertNode(head_ref, data)`  
+   - Create a new node `newNode`  
+   - Assign `data` to `newNode`  
+   - If the linked list is empty, set `head_ref` to `newNode`  
+   - Otherwise, traverse to the end and insert `newNode`  
 
-### 5. Define `bubbleSort(head)`
-   - If the list is empty, return
-   - Initialize a pointer `last = NULL`
-   - Repeat until no swaps are needed:
-     - Set `swapped = 0`
-     - Start from the `head`
-     - Traverse the linked list until reaching the last sorted node:
-       - If two adjacent nodes are out of order, swap their data using `swap(a, b)`
-       - Set `swapped = 1`
-     - Update `last` to the last checked node
+### 5. Define `bubbleSort(head)`  
+   - If the list is empty, return  
+   - Initialize a pointer `last = NULL`  
+   - Repeat until no swaps are needed:  
+     - Set `swapped = 0`  
+     - Start from `head`  
+     - Traverse the linked list until reaching the last sorted node:  
+       - If two adjacent nodes are out of order, swap their data using `swap(a, b)`  
+       - Set `swapped = 1`  
+     - Update `last` to the last checked node  
 
-### 6. Define `swap(a, b)`
-   - Swap the data values of nodes `a` and `b`
+### 6. Define `swap(a, b)`  
+   - Swap the data values of nodes `a` and `b`  
 
-### 7. Define `printList(head)`
-   - Traverse the linked list and print each node’s `data`
+### 7. Define `printList(head)`  
+   - Traverse the linked list and print each node’s `data`  
 
-### 8. End
+### 8. End  
+
 
 
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <conio.h>
+#include <stdlib.h>
 
 struct Node {
     int data;
     struct Node* next;
 };
 
-void swap(struct Node *a, struct Node *b) {
-    int temp = a->data;
-    a->data = b->data;
-    b->data = temp;
-}
-
-void bubbleSort(struct Node *head) {
-    if (head == NULL) {
-        return;
-    }
-
-    int swapped;
-    struct Node *temp;
-    struct Node *last = NULL;
-
-    do {
-        swapped = 0;
-        temp = head;
-
-        while (temp->next != last) {
-            if (temp->data > temp->next->data) {
-                swap(temp, temp->next);
-                swapped = 1;
-            }
-            temp = temp->next;
-        }
-        last = temp;
-    } while (swapped);
-}
-
-void insertNode(struct Node** head, int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+void insert(struct Node** head, int data) {
+    struct Node* temp = *head;
+    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = data;
     newNode->next = NULL;
-
     if (*head == NULL) {
-        *head = newNode;
-        return;
+	*head = newNode;
+	return;
     }
-
-    struct Node* temp = *head;
     while (temp->next != NULL) {
         temp = temp->next;
     }
     temp->next = newNode;
 }
 
-void printList(struct Node* head) {
-    printf("\nCurrent List: ");
-    while (head) {
+void print(struct Node* head) {
+    while (head != NULL) {
         printf("%d -> ", head->data);
         head = head->next;
     }
     printf("NULL\n");
 }
 
-int main() {
-    struct Node* head = NULL;
+void sort(struct Node* head) {
+    struct Node *i, *j;
+    int temp;
+    if (head == NULL) return;
 
-    clrscr();
-
-    insertNode(&head, 9);
-    insertNode(&head, 4);
-    insertNode(&head, 5);
-    insertNode(&head, 2);
-    insertNode(&head, 5);
-    insertNode(&head, 7);
-    insertNode(&head, 1);
-
-    printf("Linked list before sorting: \n");
-    printList(head);
-
-    bubbleSort(head);
-
-    printf("\nLinked list after sorting: \n");
-    printList(head);
-
-    getch();
-    return 0;
+    for (i = head; i->next != NULL; i = i->next) {
+        for (j = i->next; j != NULL; j = j->next) {
+            if (i->data > j->data) {
+                temp = i->data;
+                i->data = j->data;
+                j->data = temp;
+            }
+        }
+    }
 }
 
+void main() {
+    struct Node* head = NULL;
+    int n, data, i;
+
+    clrscr();  
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+	printf("Enter element %d (one by one): ", i + 1);
+        scanf("%d", &data);
+        insert(&head, data);
+    }
+
+    printf("\nOriginal list:\n");
+    print(head);
+
+    sort(head);
+
+    printf("\nSorted list:\n");
+    print(head);
+
+    getch();  
+}
